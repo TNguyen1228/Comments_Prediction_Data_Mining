8afe595358105dcbb48c6856a3b858b36240298b
TNguyen1228 <100148438+TNguyen1228@users.noreply.github.com>
2024-05-21 01:09:32 +0700

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,5 +2,5 @@
 client_id="H5utCvvEgZ_X28tyOuIhTw"
 client_secret="tSxc4DSHi019tQYzoF_mSdX9uWbobA"
 username='Real_Kaiezsa_128'
-password='28122002tuan' 
+password='************' 
 user_agent='kaiezsa vietnamese comments predict v0.1'
